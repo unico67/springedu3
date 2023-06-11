80d19f517ecb31629f6189966523362c3b6b6d98
unico67 <unicodaum@hanmail.net>
2023-06-11 12:48:22 +0900

    HomeController.java  수정2

--- src/main/java/com/example/springedu3/controller/HomeController.java
+++ src/main/java/com/example/springedu3/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
     FriendDaoService service;
     @GetMapping("/hello")
     public String hello() {
-        return "환영합니다~~~ HomeController 의 처리 결과입니다.!!!!";
+        return "환영합니다~~~ HomeController 의 처리 결과입니다.@@";
     }
     @GetMapping("/user")
     public String user() {
